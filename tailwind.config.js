55b4e4607e20e32352078558e12a0e2c065f3a88
Talha bayyar <93201374+TalhaBayyar@users.noreply.github.com>
2022-12-22 22:26:17 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '1313px': '1313px',
+      },
+      zIndex: {
+        '3': '3',
+      },
+
+      
+
+    },
+  },
+  plugins: [],
+}
